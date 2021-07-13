a492bfecf730b2e2597ccd55b6a4a40720405b80
Anthony Ngo <anthonyngo002@gmail.com>
2021-07-13 07:57:41 -0700

    redirects

---
+++ pages/demo/_repo/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+  async asyncData({params}) {
+    const repo = params.repo
+    return { repo }
+  },
+  created() {
+    window.location.href = `https://ngoantho.github.io/${this.repo}`
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/gh/_repo/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+  async asyncData({params}) {
+    const repo = params.repo
+    return { repo }
+  },
+  created() {
+    window.location.href = `https://github.com/ngoantho/${this.repo}`
+  },
+}
+</script>
+
+<style>
+
+</style>
